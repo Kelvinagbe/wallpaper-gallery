c6f7e942a647766960f2b5b1ebeec46689d19817
Kelvin Agbe <139677635+Kelvinagbe@users.noreply.github.com>
2026-02-11 05:49:11 +0100

    Update index.ts

--- app/components/index.ts
+++ app/components/index.ts
@@ -2,6 +2,7 @@ export { GlobalStyles } from './GlobalStyles';
 export { Header } from './Header';
 export { Navigation } from './Navigation';
 export { WallpaperCard } from './WallpaperCard';
+export { CompactWallpaperCard } from './CompactWallpaperCard';
 export { WallpaperGrid } from './WallpaperGrid';
 export { SearchModal } from './SearchModal';
 export { UploadModal } from './UploadModal';
