77a36e67f6371c5ec5de76ff5836b76167f76e90
Kelvin Agbe <139677635+Kelvinagbe@users.noreply.github.com>
2026-02-14 08:26:37 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { useAuth } from '@/components/AuthProvider';
+import { useAuth } from './components/AuthProvider';
 import { fetchWallpapers } from '@/lib/stores/wallpaperStore';
 import { Header } from './components/Header';
 import { Navigation } from './components/Navigation';
