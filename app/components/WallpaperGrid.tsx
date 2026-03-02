3a85e8bdfc1548cbc07ceb63546d17d2df907e9b
Kelvin Agbe <139677635+Kelvinagbe@users.noreply.github.com>
2026-03-02 18:51:30 +0100

    Update WallpaperGrid.tsx

--- app/components/WallpaperGrid.tsx
+++ app/components/WallpaperGrid.tsx
@@ -277,7 +277,7 @@ export const WallpaperGrid = ({
       <style>{`
         .wallpaper-grid {
           display: grid;
-          grid-template-columns: repeat(3, 1fr);
+          grid-template-columns: repeat(2, 1fr);
           gap: 14px;
           padding: 6px;
         }
