adbc67456a0552ff4400e5c547236619f8653769
Kelvin Agbe <139677635+Kelvinagbe@users.noreply.github.com>
2026-03-21 20:28:34 +0100

    Update WallpaperGrid.tsx

--- app/components/WallpaperGrid.tsx
+++ app/components/WallpaperGrid.tsx
@@ -16,8 +16,8 @@ const COLORS = [
 const CSS = `
   .wg {
     columns: 2;
-    column-gap: 10px;
-    padding: 10px 12px;
+    column-gap: 6px;
+    padding: 6px 4px;
   }
   @media(min-width:480px)  { .wg { columns: 3; column-gap: 12px; padding: 12px 16px; } }
   @media(min-width:768px)  { .wg { columns: 4; column-gap: 14px; padding: 14px 20px; } }
