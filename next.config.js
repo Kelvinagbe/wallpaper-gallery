14994c825154aea98fee934a0c47ab131c69c27b
Kelvin Agbe <139677635+Kelvinagbe@users.noreply.github.com>
2026-03-25 20:21:45 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   images: {
     remotePatterns: [
+{
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ui-avatars.com',
+      },
       {
         protocol: 'https',
         hostname: '*.public.blob.vercel-storage.com', // covers all Vercel Blob domains
